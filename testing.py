a9e124661f9d0f6cd38389d8bc5b6a3bedb66ce5
mohith-git <67273359+mohith-git@users.noreply.github.com>
2022-07-07 12:41:36 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -148,7 +148,195 @@ def Contact_Order(f):
             i1=i1+1
         return(summ/len(xa))
 
+def disulphide_interaction(f):
+        
+        fi = read_file(f)
+        cys1,cys = [],[]
+        for i in fi:
+            aa=i.split()
+            if aa[0]=='ATOM' and aa[4]=='A' and aa[2]=='SG':
+                cys1.append(aa)
+            if aa[0] == 'MODEL' and aa[1] == '2':
+                break
+        
+        for i in cys1:
+            if i[3]=='CYS':
+                cys.append(i)
+        
+        p=[]
+        i1=0
+        for i in cys:
+            j1=0
+            for j in cys:
+                d=distance_formula(i,j)
+                if 0<d**0.5<=2.2 and [j[3],j[5],i[3],i[5],d**0.5] not in p:
+                    p.append([i[3],i[5],j[3],j[5],d**0.5])
+                j1=j1+1
+            i1=i1+1
+        return(p)
+def Hydrophobic_interaction(f):
+        fi = read_file(f)
+        p,xb=[],[]
+        i1=0
+        for i in fi:
+            aa=i.split()
+            if aa[0]=='ATOM' and aa[4]=='A' and aa[2]=='CB':
+                xb.append(aa)
+            if aa[0] == 'MODEL' and aa[1] == '2':
+                break
+        for i in xb:
+          if i[3] in ['ALA','VAL','LEU','TYR','PRO','TRP','MET','PHE','ILE']:
+            j1=0
+            for j in xb:
+              if j[3] in ['ALA','VAL','LEU','TYR','PRO','TRP','MET','PHE','ILE']:
+                d=distance_formula(i,j)
+                if 0<d**0.5<=5 and [j[3],j[5],i[3],i[5],d**0.5] not in p:
+                    p.append([i[3],i[5],j[3],j[5],d**0.5])
+                j1=j1+1
+            i1=i1+1
+        return(p)
+        
 
+    
+def Main_Main_H_Bond(f):
+        fi = read_file(f)
+        m_n,m_s,m_o=[],[],[]
+        for i in fi:
+            aa=i.split()
+            if aa[0]=='ATOM' and aa[4]=='A' and aa[2] in ['N','NE','NH1','NH2','NZ','ND1','NE2','NE1','ND2']:
+                m_n.append(aa)
+            elif aa[0]=='ATOM' and aa[4]=='A' and aa[2] in ['O','OE2','OE1','OD1','OD2','OG1','OG2','OH','OG']:
+                m_o.append(aa)
+            elif aa[0]=='ATOM' and aa[4]=='A' and aa[2]==['SG','SD']:
+                m_s.append(aa)
+            if aa[0] == 'MODEL' and aa[1] == '2':
+                break
+        h_bond = []
+        i1=0
+        for i in m_n:
+         if i[2] == 'N':
+            j1=0
+            
+            for j in m_o:
+              if j[2] == 'O':
+               if abs(int(i[5]) - int(j[5])) > 1:
+                d=distance_formula(i,j)
+                if 0<d**0.5<3.5 and [j[3],j[5],j[2],i[3],i[5],i[2],round(d**0.5,2)] not in h_bond:
+                    h_bond.append([i[3],i[5],i[2],j[3],j[5],j[2],round(d**0.5,2)])
+                j1=j1+1
+            i1=i1+1
+        h_bond = sorted(h_bond,key=lambda x : x[1])
+        return((h_bond))
+    
+    
+def Main_Side_Chain_H_Bond(f):
+        fi = read_file(f)
+        m_n,m_s,m_o=[],[],[]
+        for i in fi:
+            aa=i.split()
+            if aa[0]=='ATOM' and aa[4]=='A' and aa[2] in ['N','NE','NH1','NH2','NZ','ND1','NE2','NE1','ND2']:
+                m_n.append(aa)
+            elif aa[0]=='ATOM' and aa[4]=='A' and aa[2] in ['O','OE2','OE1','OD1','OD2','OG1','OG2','OH','OG']:
+                m_o.append(aa)
+            elif aa[0]=='ATOM' and aa[4]=='A' and aa[2]==['SG','SD']:
+                m_s.append(aa)
+            if aa[0] == 'MODEL' and aa[1] == '2':
+                break
+        
+        atoms= [*m_n,*m_o,*m_s]
+        h_bond = []
+        real = []
+        i1=0
+        for i in atoms:
+           
+            j1=0
+            
+            
+            for j in atoms:
+             
+               if abs(int(i[5]) - int(j[5])) > 1:
+                
+                 d=distance_formula(i,j)
+                
+                 if 0<d**0.5<3.5:
+                    if i[2] == 'O':
+                      h_bond.append([j[3],j[5],j[2],i[3],i[5],i[2],round(d**0.5,2)])
+                    else:
+                        if j[2] == 'N':
+                          h_bond.append([j[3],j[5],j[2],i[3],i[5],i[2],round(d**0.5,2)])   
+                     
+               j1=j1+1
+            i1=i1+1
+        for i in h_bond:
+           if i[2] in ['N','O'] and i[5]in ['N','O']:
+               pass
+           elif i[2] in ['N','O'] or i[5]in ['N','O']:
+               real.append(i)
+        real = sorted(real,key=lambda x : x[1])
+        return(((real)))   
+    
+    
+    
+def Side_Side_Chain_H_Bond(f):
+        fi = read_file(f)
+        m_n,m_s,m_o=[],[],[]
+        for i in fi:
+            aa=i.split()
+            if aa[0]=='ATOM' and aa[4]=='A' and aa[2] in ['N','NE','NH1','NH2','NZ','ND1','NE2','NE1','ND2']:
+                m_n.append(aa)
+            elif aa[0]=='ATOM' and aa[4]=='A' and aa[2] in ['O','OE2','OE1','OD1','OD2','OG1','OG2','OH','OG']:
+                m_o.append(aa)
+            elif aa[0]=='ATOM' and aa[4]=='A' and aa[2]==['SG','SD']:
+                m_s.append(aa)
+            if aa[0] == 'MODEL' and aa[1] == '2':
+                break
+        atoms= [*m_n,*m_o,*m_s]
+        h_bond = []
+        
+        i1=0
+        for i in atoms:
+           if i[2] not in ['N','O']:
+            j1=0
+            
+            
+            for j in atoms:
+              if j[2] not in ['N','O']:
+               if abs(int(i[5]) - int(j[5])) > 1:
+                d=distance_formula(i,j)
+                if 0<d**0.5<3.5 and [j[3],j[5],j[2],i[3],i[5],i[2],round(d**0.5,2)] not in h_bond:
+                    h_bond.append([i[3],i[5],i[2],j[3],j[5],j[2],round(d**0.5,2)])
+                j1=j1+1
+            i1=i1+1
+        
+        return(((h_bond)))
+    
+def Ionic_Interaction(f):
+        fi = read_file(f)
+        cation,anion=[],[]
+        for i in fi:
+            aa=i.split()
+            if aa[0]=='ATOM' and aa[4]=='A' and aa[2] in ['NH1','NH2','NZ','ND1','NE2'] and aa[3] in ['ARG','LYS','HIS']:
+                cation.append(aa)
+            if aa[0]=='ATOM' and aa[4]=='A' and aa[2] in ['OE2','OE1','OD1','OD2'] and aa[3] in ['ASP','GLU']:
+                anion.append(aa)
+            if aa[0] == 'MODEL' and aa[1] == '2':
+                break  
+        
+        ion = []
+        i1=0
+        for i in cation:
+            j1=0
+           
+            for j in anion:
+                d=distance_formula(i,j)
+                if 0<d**0.5<6:
+                    ion.append([i[3],i[5],i[2],j[3],j[5],j[2],round(d**0.5,2)])
+                j1=j1+1
+            i1=i1+1
+         
+        
+        
+        return(ion)
 # @app.route("/",methods=['POST','GET'])
 # def first():
 #     if request.method=='POST':
@@ -200,9 +388,11 @@ def contact_order(f):
           
           return render_template('co.html',y=Contact_Order(f))
 
-# @app.route("/ion/")
-# def ionic_interaction():
-#         return render_template('ion.html',y=)
+@app.route("/ion/")
+@app.route("/ion/<f>")
+def ionic_interaction(f):
+    
+        return render_template('ion.html',y='Ionic_Interaction(f)')
 
 
 
